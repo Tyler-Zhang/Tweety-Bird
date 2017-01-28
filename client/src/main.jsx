6cfc1b2ccd7d99488c5b25514444b85481f9c7e9
Tyler-Zhang <TZTylerzhang@gmail.com>
2017-01-28 13:27:24 -0500

    Updated gitignore

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App {
+class App extends React.Component{
     render(){
-
+        return (
+            <div class="container-fluid" style={{width: "100%", height: "100%"}}>
+                <h1 style={{margin: "100px"}}> Enter a topic below:</h1>
+                <div class="col-md-8 col-md-offset-2">
+                </div>
+            </div>
+        )
     }
 }
+
+reactDOM.render(<App/>, document.getElementById("root"));
