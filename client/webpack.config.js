59a36901c6d3f79a0570ea20da4e351f67f299de
Tyler-Zhang <TZTylerzhang@gmail.com>
2017-01-28 12:27:49 -0500

    Client side bare bones

---
+++ client/webpack.config.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+const path = require("path");
+const debug = true;
+
+var prodPlugins = [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console:true
+            },
+            output: {
+                comments: false,
+            }
+        })
+];
+let plugins;
+
+if(!debug)
+    plugins = prodPlugins;
+
+ module.exports = {
+     entry: {
+        content:'./src/main.jsx',
+    },
+     output: {
+         path: './bin',
+         filename: '[name].bundle.js'
+     },
+     resolve: {
+         extensions: ["", ".js", ".jsx", ".scss", ".css"],
+         modulesDirectories: [
+             path.resolve("./src/components"),
+             path.resolve("./node_modules")
+         ]
+     },
+     watch: debug,
+     module: {
+         loaders: [
+             {
+             test: /\.jsx?$/,
+             exclude: /node_modules/,
+             loader: 'babel',
+             query: {
+                 presets:['react', 'es2015', 'stage-0'],
+                 plugins: ['react-html-attrs']
+                }
+            },
+            {
+                test:/\.s?css$/,
+                loaders:['style', 'css', 'sass']
+            },
+            {  
+                test: /\.svg$/, 
+                loader: 'svg-loader?pngScale=2'},
+            {
+                test   : /\.(png|jpg)$/,
+                loader : 'url-loader?limit=8192'
+            },
+            {
+                test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader : 'file-loader?name=./../files/[hash].[ext]'
+            }]
+     }, plugins
+ };
