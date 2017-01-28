b6657e3cc7fdd479e3c3c68bf9d163c694f8b78a
Hassaan Sami <hassaan.sami@live.com>
2017-01-28 18:15:39 -0500

    update styled web files with resources

--- client/src/components/input.jsx
+++ client/src/components/input.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default class Input extends React.Component{
-    
+
     submit(){
         let input = document.getElementById("inputText");
         let text = input.value;
@@ -12,11 +12,20 @@ export default class Input extends React.Component{
     render(){
         return (
             <div class="container-fluid" style={{width: "100%", height: "100%"}}>
-                <h1 style={{margin: "100px"}}> Enter a topic below:</h1>
-                <div class="col-md-8 col-md-offset-2 form-group">
-                    <input id="inputText" class="form-control" type="text" placeholder="abortion"/>
-                    <button class="btn btn-success center-block" onClick={() => this.submit()}> Analyze </button>
+
+                <h1 id="prompt">     ENTER A TOPIC BELOW:
+                </h1>
+
+                <div id="form" class="col-md-8 col-md-offset-2 form-group">
+
+                    <input id="inputText" class="form-control" type="text" placeholder="e.g. abortion"/>
+
+                    <button id="btnAnalyze" class="btn center-block" onClick={() => this.submit()}>
+                        Analyze
+                    </button>
+
                 </div>
+
             </div>
         )
     }
