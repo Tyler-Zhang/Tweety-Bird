611cc72b4b78f810811b68318c7b46090069c347
Hassaan Sami <hassaan.sami@live.com>
2017-01-28 20:34:35 -0500

    Update web design and add resources

--- client/src/components/input.jsx
+++ client/src/components/input.jsx
@@ -13,12 +13,11 @@ export default class Input extends React.Component{
         return (
             <div class="container-fluid" style={{width: "100%", height: "100%"}}>
 
-                <h1 id="prompt">     ENTER A TOPIC BELOW:
-                </h1>
+                <h1 id="prompt">ENTER A <div id="topicSpan">TOPIC</div> BELOW:</h1>
 
                 <div id="form" class="col-md-8 col-md-offset-2 form-group">
 
-                    <input id="inputText" class="form-control" type="text" placeholder="e.g. abortion"/>
+                    <input id="inputText" class="form-control" type="text" placeholder="e.g. elections"/>
 
                     <button id="btnAnalyze" class="btn center-block" onClick={() => this.submit()}>
                         Analyze
